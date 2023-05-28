0ed4cc3fd3807f0974d72942b48b07e2b654e0af
Nadav-Katz <kanadav@gmail.com>
2023-05-28 12:21:49 +0300

    package

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='Arxiv_Authors_Net',
+    name='AuthorsNet',
     url='https://github.com/Nadav-Katz/arxiv-authors-net',
     author="Nadav Katz",
     author_email='nadav.katz3@mail.huji.ac.il',
     # Needed to actually package something
     packages=['authors_net'],
     # Needed for dependencies
-    install_requires=['pandas', 'pandas', 'networkx', 'tqdm'],
+    install_requires=['pandas', 'networkx', 'tqdm'],
     # *strongly* suggested for sharing
     version='0.1',
     description='An example of a python package from pre-existing code',
