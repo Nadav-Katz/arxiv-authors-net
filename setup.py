fe7f6e1593e5e4ca7175a8483fcc980ddee1a9c0
Nadav-Katz <81093069+Nadav-Katz@users.noreply.github.com>
2023-06-21 00:21:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
 from setuptools import setup
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
     name='AuthorsNet',
     url='https://github.com/Nadav-Katz/arxiv-authors-net',
     author="Nadav Katz",
     author_email='nadav.katz3@mail.huji.ac.il',
-    # Needed to actually package something
     packages=['authors_net'],
     # Needed for dependencies
     install_requires=['pandas', 'networkx', 'tqdm'],
-    # *strongly* suggested for sharing
-<<<<<<< HEAD
     version='0.2',
-    description='An python package for craeting Arxiv Co-Authorship network',
-=======
-    version='0.1',
     description='A code for creating a authors collaboration network from the arxiv dataset',
->>>>>>> 84663c0fb5c2e2188eb1b6ca3ebc6d14657f787d
-    # We will also need a readme eventually (there will be a warning)
+    # Readme
     long_description=open('README.md').read(),
 )
