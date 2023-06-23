9fcf664a2c1b4c4b44587adb6d50088aef75e40e
Nadav-Katz <kanadav@gmail.com>
2023-06-23 18:40:52 +0300

    add Thread lock

--- extra/example.py
+++ extra/example.py
@@ -4,22 +4,20 @@ import networkx as nx
 
 # create net: 
 network_nt = AuthorsNetwork(filename='arxiv-metadata-oai-snapshot.json',
-                            max_rows=None,
+                            max_rows=100000,
                             chunk_size=100,
                             extra_edge_features=False, 
-                            num_consumers=1, num_producers=1)
+                            num_consumers=3, num_producers=1)
 # create network:
 df = network_nt.build_network_df()
 
 # write to pkl:
 pd.to_pickle(df, "colab_net.pkl", compression='gzip')
 
-print(df.shape)
+# # some interesting cases: 
+# df.sort_values(by='paper_count', ascending=False).head(10)
 
-# some interesting cases: 
-df.sort_values(by='paper_count', ascending=False).head(10)
+# # convert to graph: 
+# g = network_nt.to_networkx()
 
-# convert to graph: 
-g = network_nt.to_networkx()
-
-print(f'The network has {len(g.edges)} edges and {len(g.nodes)} nodes')
+# print(f'The network has {len(g.edges)} edges and {len(g.nodes)} nodes')
